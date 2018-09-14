2f84114ab88973f575901537b5a72a5edfc3d0ee
max cheung <maxcheung@optusnet.com.au>
2018-09-14 22:35:14 +1000

    bootstrap

---
+++ src/main/java/org/baeldung/SalesController.java
@@ -0,0 +1,28 @@
+package org.baeldung;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RestController
+@RequestMapping("sales")
+public class SalesController {
+
+	private static final Logger LOG = LoggerFactory.getLogger(SalesController.class);
+
+
+	@RequestMapping(value = "/getSalesX", method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE,
+			MediaType.APPLICATION_XML_VALUE })
+	public @ResponseBody String getSalesX() {
+		LOG.info(" getSales");
+		return "Sales";
+	}
+
+
+}
